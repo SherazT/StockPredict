b559b510a7f0784ade23ea42d34621dffc0833af
Sheraz Tariq <sheraztariq@hotmail.com>
2016-12-26 16:35:54 -0500

    adjusted dataframe with columns needed

--- linearRegression.py
+++ linearRegression.py
@@ -3,4 +3,13 @@ import quandl
 
 df = quandl.get('WIKI/GOOGL')
 
+df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
+
+
+df['Percent_Volatility'] = ((df['Adj. High'] - df['Adj. Low'])/ df['Adj. Low'])* 100.0
+
+df['Percent_Change'] = ((df['Adj. Close'] - df['Adj. Open'])/ df['Adj. Open'])* 100.0
+
+df = df[['Adj. Close', 'Percent_Volatility', 'Percent_Change', 'Adj. Volume']]
+
 print(df.head())
