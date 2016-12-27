e3aae6eeac3f4eab8bdcaa2df77e92256ac64317
Sheraz Tariq <sheraztariq@hotmail.com>
2016-12-26 20:42:11 -0500

    setup prediction column

--- linearRegression.py
+++ linearRegression.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import quandl
+import math
 
 df = quandl.get('WIKI/GOOGL')
 
@@ -11,5 +12,13 @@ df['Percent_Volatility'] = ((df['Adj. High'] - df['Adj. Low'])/ df['Adj. Low'])*
 df['Percent_Change'] = ((df['Adj. Close'] - df['Adj. Open'])/ df['Adj. Open'])* 100.0
 
 df = df[['Adj. Close', 'Percent_Volatility', 'Percent_Change', 'Adj. Volume']]
+
+forecast_column = 'Adj. Close'
+
+df.fillna(-99999, inplace=True) #replace NaN values with a really small number to make it an outlier instead
+
+forecast_offset = int(math.ceil(0.01*len(df))) #predict 10% of dataframe
+
+df['prediction'] = df[forecast_column].shift(-forecast_offset) #initilize prediction column with data from 10% days ago
 
 print(df.head())
