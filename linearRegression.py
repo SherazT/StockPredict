c50e50ed5fddd2c0343aa91c378db741bc5675f4
Sheraz Tariq <sheraztariq@hotmail.com>
2016-12-30 00:30:15 -0500

    setup for numerical predictions

--- linearRegression.py
+++ linearRegression.py
@@ -4,6 +4,7 @@ import math
 import numpy as np
 from sklearn import preprocessing, model_selection, svm
 from sklearn.linear_model import LinearRegression
+import pdb
 
 df = quandl.get('WIKI/GOOGL')
 
@@ -25,20 +26,20 @@ forecast_offset = int(math.ceil(0.01*len(df))) #predict 10% of dataframe
 
 df['prediction_for_10_days_after'] = df[forecast_column].shift(-forecast_offset) #initilize prediction column by shifting Adj. Close column 10% up
 
-df.dropna(inplace=True)
-
 #features
 X = np.array(df.drop(['prediction_for_10_days_after'], axis=1)) #convert to numpy array and drop prediction column 
-
-#predictions/labels
-y = np.array(df['prediction_for_10_days_after'])
-
 #normalize features
+
 X = preprocessing.scale(X)
+X_lately = X[-forecast_offset:] #hold all of the inputs that don't have a prediction for 10 days after (it's a NaN value)
+X = X[:-forecast_offset:] #holds all the inputs where there is a prediction (just the value shifted 10 days up) for 10 days later
+
+pdb.set_trace()
+
 
 df.dropna(inplace=True)
-print(len(X), len(y))
-
+#predictions/labels
+y = np.array(df['prediction_for_10_days_after'])
 
 #setup training and testing sets
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
@@ -48,4 +49,3 @@ classifer = LinearRegression()
 classifer.fit(X_train, y_train) #run gradient decent and fit parameters 
 accuracy = classifer.score(X_test, y_test)
 
-print(accuracy)
