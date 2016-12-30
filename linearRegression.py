3ebb371af02301c05925b18b9ced67ed8f0f8830
Sheraz Tariq <sheraztariq@hotmail.com>
2016-12-30 02:02:21 -0500

    setup visualization

--- linearRegression.py
+++ linearRegression.py
@@ -1,15 +1,18 @@
 import pandas as pd
-import quandl
-import math
+import quandl, time
+import math, datetime
 import numpy as np
 from sklearn import preprocessing, model_selection, svm
 from sklearn.linear_model import LinearRegression
 import pdb
+import matplotlib.pyplot as plt 
+from matplotlib import style
+
+style.use('ggplot')
 
 df = quandl.get('WIKI/GOOGL')
 
 df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
-
 
 df['Percent_Volatility'] = ((df['Adj. High'] - df['Adj. Low'])/ df['Adj. Low'])* 100.0
 
@@ -23,7 +26,6 @@ df.fillna(-99999, inplace=True) #replace NaN values with a really small number t
 
 forecast_offset = int(math.ceil(0.01*len(df))) #predict 10% of dataframe
 
-
 df['prediction_for_10_days_after'] = df[forecast_column].shift(-forecast_offset) #initilize prediction column by shifting Adj. Close column 10% up
 
 #features
@@ -34,10 +36,11 @@ X = preprocessing.scale(X)
 X_lately = X[-forecast_offset:] #hold all of the inputs that don't have a prediction for 10 days after (it's a NaN value)
 X = X[:-forecast_offset:] #holds all the inputs where there is a prediction (just the value shifted 10 days up) for 10 days later
 
-pdb.set_trace()
+#pdb.set_trace() #byebug sub for python
 
 
-df.dropna(inplace=True)
+df.dropna(inplace=True) #X no longer contains rows that X_lately has 
+
 #predictions/labels
 y = np.array(df['prediction_for_10_days_after'])
 
@@ -49,3 +52,20 @@ classifer = LinearRegression()
 classifer.fit(X_train, y_train) #run gradient decent and fit parameters 
 accuracy = classifer.score(X_test, y_test)
 
+forecast_set = classifer.predict(X_lately) #based on these inputs X, find y. currently no known y values
+
+#print(forecast_set, accuracy, forecast_offset)
+
+df['Forecast'] = np.nan #empty column
+
+last_date = df.iloc[-1].name #last date in dataframe (one of the dates that was predicted in line 55)
+last_unix = time.mktime(last_date.to_pydatetime().timetuple())	#unix timestamp of the last date 
+one_day = 86400	#no of seconds in a day
+next_unix = last_unix + one_day	
+
+pdb.set_trace()
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += 86400
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)]+[i]
