c16b1efd9298c1b3c290110b99f3252327f08701
Sheraz Tariq <sheraztariq@hotmail.com>
2017-01-01 18:28:56 -0500

    setup pickling

--- linearRegression.py
+++ linearRegression.py
@@ -7,6 +7,7 @@ from sklearn.linear_model import LinearRegression
 import pdb
 import matplotlib.pyplot as plt 
 from matplotlib import style
+import pickle
 
 style.use('ggplot')
 
@@ -28,7 +29,7 @@ forecast_offset = int(math.ceil(0.01*len(df))) #predict 10% of dataframe
 
 df['prediction_for_10_days_after'] = df[forecast_column].shift(-forecast_offset) #initilize prediction column by shifting Adj. Close column 10% up
 
-#features
+#featuresg
 X = np.array(df.drop(['prediction_for_10_days_after'], axis=1)) #convert to numpy array and drop prediction column 
 #normalize features
 
@@ -52,10 +53,16 @@ y = y[:-forecast_offset:]
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
 
 #setup classifer 
-classifer = LinearRegression()
-classifer.fit(X_train, y_train) #run gradient decent and fit parameters 
-accuracy = classifer.score(X_test, y_test)
+# classifer = LinearRegression()
+# classifer.fit(X_train, y_train) #run gradient decent and fit parameters 
 
+# with open('linearRegression.pickle', 'wb') as f:
+# 	pickle.dump(classifer, f)
+
+pickle_in = open('linearRegression.pickle', 'rb')
+classifer = pickle.load(pickle_in)
+
+accuracy = classifer.score(X_test, y_test)
 forecast_set = classifer.predict(X_lately) #based on these inputs X, find y. currently no known y values
 
 #print(forecast_set, accuracy, forecast_offset)
@@ -74,9 +81,11 @@ for i in forecast_set:
     next_date = datetime.datetime.fromtimestamp(next_unix)
     next_unix += 86400
     df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)]+[i]
+#pdb.set_trace()
 
 print(y[-32:] ,forecast_set, accuracy, forecast_offset) #output actual results from 32 days ago and current predictions to make a comparision
-
+#datetime.datetime(2017, 1, 30, 0, 0)
+#pdb.set_trace()
 
 df['Adj. Close'].plot()
 df['Forecast'].plot()
